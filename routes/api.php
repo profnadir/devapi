9cfa825ae8cb0b9f88b278154b9ef2e4e0d83574
Hamza Nadir <hamzanadir.dev@gmail.com>
2023-03-08 16:44:35 +0100

    implements protected routes using sunctum

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\APIAuthController;
 use App\Http\Controllers\ProductsController;
 
 /*
@@ -15,8 +16,21 @@ use App\Http\Controllers\ProductsController;
 |
 */
 
+
+
+
+// public routes
+Route::post('/register', [APIAuthController::class, 'register']);
+Route::post('/login', [APIAuthController::class, 'login']);
 Route::get('/products/search/{name}',[ProductsController::class,'search']);
-Route::apiResource('/products',ProductsController::class);
+Route::apiResource('/products',ProductsController::class)->only(['index','show']);
+
+
+// protected routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('/products',ProductsController::class)->only(['store','update','destroy']);
+    Route::post('/logout', [APIAuthController::class, 'logout']);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
