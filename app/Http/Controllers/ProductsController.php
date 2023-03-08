a1daaca4bc186f41e56075d74d15e8e295a76e83
Hamza Nadir <hamzanadir.dev@gmail.com>
2023-03-08 16:04:10 +0100

    implements public routes

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Product::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate(
+            [
+                'name' => 'required',
+                'slug' => 'required',
+                'description' => 'required',
+                'price' => 'required'
+            ]
+        );
+
+        return Product::create($validated);
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        return $product;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+        $validated = $request->validate(
+            [
+                'name' => 'required',
+                'slug' => 'required',
+                'description' => 'required',
+                'price' => 'required'
+            ]
+        );
+
+        $product->update($validated);
+
+        return $product;
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        return $product->delete() ?  'ok' :  'ko';
+    }
+
+    public function search($name)
+    {
+        return Product::where('name','like','%'.$name.'%')->get();
+    }
+}
